f1e62b18a7e9d12e7168764387fdb1707258716d
Sam Adams <sam.adams7785@gmail.com>
2022-09-15 15:29:57 -0700

    Fixed Attractions, working city name display

--- app.js
+++ app.js
@@ -6,56 +6,69 @@ const climate = document.getElementById('select-climate');
 const architecture = document.getElementById('select-architecture');
 const attractionInput = document.getElementById('input-attraction');
 const addButton = document.getElementById('add-button');
-const attractionList = document.getElementById('input-attraction');
-const cityDrawing = document.getElementById('cityDrawing');
+const cityDisplay = document.getElementById('city-display');
+const NameDisplay = document.getElementById('name-display');
+const attractionList = document.getElementById('list-attraction');
+
 /* State */
 
 const city = {
-    name: 'default',
-    climate: 'default',
-    architecture: 'default',
+    name: '???',
+    climate: 'Tropical',
+    architecture: 'Medieval',
     attraction: [],
 };
 
 cityName.addEventListener('input', () => {
-    cityName.value = city.name;
+    city.name = cityName.value; // I didn't know that the input had to go first, im learning wow
     displayCity();
 });
 
 // EVENT LISTENER FOR CLIMATE
 climate.addEventListener('change', () => {
-    climate.input = city.climate;
+    city.climate = climate.input;
     displayCity();
 });
 
 // EVENT LISTENER FOR architecture
 architecture.addEventListener('change', () => {
-    architecture.input = city.architecture;
+    city.architecture = architecture.input;
     displayCity();
 });
 
+cityName.value = city.name;
+climate.value = city.climate;
+architecture.value = city.architecture;
+
+function displayCity() {
+    cityDisplay.classList.value = '';
+    cityDisplay.classList.add(city.climate, city.architecture);
+    NameDisplay.textContent = city.name;
+}
+
 /* Events */
-addButton.addEventListener('click', () => {
-    const attraction = attractionInput.value;
-    // No blank attractions
-    if (attraction.value === '') {
-        return;
-    }
-    city.attraction.push(attraction);
-    displayAttractions();
-});
 
 function displayAttractions() {
     attractionList.innerHTML = '';
 
-    for (const attraction of city.attractions) {
+    for (const attraction of city.attraction) {
         const li = document.createElement('li');
         li.textContent = attraction;
         attractionList.append(li);
     }
 }
 
+addButton.addEventListener('click', () => {
+    const attraction = attractionInput.value;
+    // No blank attractions
+    // if (attraction.value === '') {
+    //     return;
+    // }
+    city.attraction.push(attraction);
+    displayAttractions();
+});
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
 displayCity();
+displayAttractions();
